b42f00898bfe92b0de749540a2e05d1974d1bc5a
Diego Lamilla <lamilla.tenazoa@gmail.com>
2026-01-30 22:49:03 -0500

    DoTween

---
+++ Assets/Scripts/Lamilla/MenuOoptions.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOoptions : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
